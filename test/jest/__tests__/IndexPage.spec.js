fd856613c7a289db9772fb00e140dfde34eaa607
Sunisa-Phadan <Chi99141@gmail.com>
2024-02-06 18:10:16 +0700

    jest-test edit

--- test/jest/__tests__/IndexPage.spec.js
+++ test/jest/__tests__/IndexPage.spec.js
@@ -1,3 +1,4 @@
+// IndexPage.spec.js
 import IndexPage from 'src/pages/IndexPage.vue'
 import { shallowMount } from '@vue/test-utils'
 
@@ -7,9 +8,8 @@ describe('IndexPage', () => {
     let header = wrapper.find('.htmlClass h1')
     expect(header.exists()).toBe(true)
     expect(header.text())
-      .toBe('Vue is awesome.')
+      .toBe('Sunisa Phadan 6404101349')
   })
-  
   it('check text content to be as defined in variable', () => {
     const wrapper = shallowMount(IndexPage, {
       data () {
@@ -22,4 +22,3 @@ describe('IndexPage', () => {
     expect(header.text()).toBe('I love Vue.')
   })
 })
-
